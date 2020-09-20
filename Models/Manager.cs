2fc35658bef57624e2998ba116279a8782509e08
beraatkoyuncu <70577470+beraatkoyuncu@users.noreply.github.com>
2020-09-20 21:55:03 +0300

    son

---
+++ Models/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public class Manager
+    {
+        public int ID { get; set; }
+        [Display(Name = "Aktivite İsmi")]
+        public string ActivityName { get; set; }
+        [Display(Name = "Aktivite Tarihi")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Aktivite Saati")]
+        [DataType(DataType.Time)]
+        public DateTime ReleaseTime { get; set; }
+        [Display(Name = "Aktivite Açıklaması")]
+        public string ActivityExplanation { get; set; }
+
+    }
+}
---
+++ Pages/Managers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Data;
+using TaskManager.Models;
+
+namespace TaskManager.Pages.Managers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly TaskManager.Data.TaskManagerContext _context;
+
+        public IndexModel(TaskManager.Data.TaskManagerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Manager> Manager { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Manager = await _context.Manager.ToListAsync();
+        }
+    }
+}
